832b6a14dbf27f01294afd5aea82735dd63fecc2
Simon Coker <kornerstonecoker@gmail.com>
2024-03-31 00:55:26 +0000

    FYP Assets added

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -3,6 +3,24 @@ import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
 
+/**
+ * <summary>
+ * This TypeScript function handles POST requests to create a new billboard entry associated with a
+ * specific store ID after performing authentication and input validation checks.
+ * </summary>
+ * <param name="req">The `req` parameter in the code snippet represents the incoming request object in
+ * the POST function. It is used to access and handle the data sent in the request, such as the request
+ * body (using `req.json()`), headers, and other request details. The `req` object is typically
+ * provided</param>
+ * <param name="">The code you provided is a TypeScript function for handling a POST request. Here is
+ * an explanation of the parameters used in the function:</param>
+ * <returns>
+ * The code is returning a JSON response with the created billboard data if the request is successful.
+ * If there are any validation errors or authorization issues, appropriate error messages with
+ * corresponding HTTP status codes are returned. In case of any internal server error, a generic
+ * "Internal error" message with a status code of 500 is returned.
+ * </returns>
+ */
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
